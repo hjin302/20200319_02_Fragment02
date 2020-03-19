9929cafd169352ac0987317db9fbe7fcd14320dd
hjin302 <enro302@naver.com>
2020-03-19 14:47:27 +0900

    방 목록 한줄 xml 생성

---
+++ app/src/main/java/com/example/a20200319_02_fragment02/datas/Room.java
@@ -0,0 +1,22 @@
+package com.example.a20200319_02_fragment02.datas;
+
+import java.io.Serializable;
+
+public class Room implements Serializable {
+
+//    가격, 주소만 표현
+    private int price;
+    private String address;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Room(int price) {
+        this.price = price;
+    }
+}
