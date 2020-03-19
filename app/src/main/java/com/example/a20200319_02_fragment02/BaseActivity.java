ce5cda6ac7095d19ea8261d41b2243f50d7c243c
hjin302 <enro302@naver.com>
2020-03-19 12:33:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20200319_02_fragment02/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.a20200319_02_fragment02;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// abstract 메쏘드를 가지는 클래스는 반드시 abstract class
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents(); //이벤트 세팅
+    public abstract void setValues(); // 값 세팅
+
+}
