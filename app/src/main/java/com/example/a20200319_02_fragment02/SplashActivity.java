80a4f5df3bfcb079e89a5042b7df81277dff7f7a
hjin302 <enro302@naver.com>
2020-03-19 14:39:09 +0900

    방 목록 프래그먼트 생성

---
+++ app/src/main/java/com/example/a20200319_02_fragment02/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.example.a20200319_02_fragment02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.a20200319_02_fragment02.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends BaseActivity {
+
+    ActivitySplashBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(mContext, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 2000);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a20200319_02_fragment02/fragments/RoomListFragment.java
@@ -0,0 +1,33 @@
+package com.example.a20200319_02_fragment02.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a20200319_02_fragment02.R;
+import com.example.a20200319_02_fragment02.databinding.FragmentRoomListBinding;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+public class RoomListFragment extends Fragment {
+
+    FragmentRoomListBinding binding = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_room_list, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        프래그먼트의 동작 코드 => 리스트뷰 띄워주기
+    }
+}
